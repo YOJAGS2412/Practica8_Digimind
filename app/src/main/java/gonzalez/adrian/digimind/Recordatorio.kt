bf20c88d132eb97fe83270c1cb04fc4e348184a0
YOJAGS2412 <89612223+YOJAGS2412@users.noreply.github.com>
2021-10-21 16:14:43 -0700

    primera version

---
+++ app/src/main/java/gonzalez/adrian/digimind/Recordatorio.kt
@@ -0,0 +1,7 @@
+package gonzalez.adrian.digimind
+
+import java.io.Serializable
+
+data class Recordatorio  (var dia:String,
+                          var tiempo:String,
+                          var nombre:String) :Serializable
---
+++ app/src/main/java/gonzalez/adrian/digimind/ui/Task.kt
@@ -0,0 +1,5 @@
+package gonzalez.adrian.digimind.ui
+
+class Task  (var tittle:String,
+             var days:ArrayList<String>,
+             var time: String)
